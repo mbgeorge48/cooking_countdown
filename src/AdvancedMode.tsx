6eab56197105c859f03cc747bb028360fb68ccef
Matt George <mbgeorge48@gmail.com>
2023-12-23 09:47:27 +0000

    Create empty advancedmode component

---
+++ src/AdvancedMode.tsx
@@ -0,0 +1,20 @@
+export async function AdvancedMode() {
+    // Render a simple formik form that has a single text box
+    // When the user submits that form you can do something like this
+    //   const handleSubmit = (values: Values) => {
+    //       const response = await fetch(url, {
+    //         method: 'POST',
+    //         headers: {
+    //           'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({ timer data }),
+    //       });
+    //       const responseData = await response.json();
+    //       setResponse(responseData);
+    //   };
+
+    // The idea is it's going to be a fire and forget
+    // Maybe we do want to catch the response in case it is successful?
+
+    return <></>;
+}
