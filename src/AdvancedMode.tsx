37eebf42bdd7597fefea1bea514a11113b13f87a
Matt George <mbgeorge48@gmail.com>
2023-12-23 17:39:45 +0000

    Added the experimental post data component

--- src/AdvancedMode.tsx
+++ src/AdvancedMode.tsx
@@ -1,20 +1,62 @@
-export async function AdvancedMode() {
-    // Render a simple formik form that has a single text box
-    // When the user submits that form you can do something like this
-    //   const handleSubmit = (values: Values) => {
-    //       const response = await fetch(url, {
-    //         method: 'POST',
-    //         headers: {
-    //           'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify({ timer data }),
-    //       });
-    //       const responseData = await response.json();
-    //       setResponse(responseData);
-    //   };
+import { Field, Form, Formik } from "formik";
+import { useState } from "react";
 
-    // The idea is it's going to be a fire and forget
-    // Maybe we do want to catch the response in case it is successful?
+import { Timer } from "./types";
 
-    return <></>;
+interface Props {
+    timerData: Timer[];
+}
+
+export function AdvancedMode(props: Props) {
+    const [responseOk, setResponseOk] = useState<boolean>(false);
+    const initialValues = { url: "" };
+
+    const handleSubmit = async (values: { url: string }) => {
+        console.log({ values });
+        const response = await fetch(values.url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(props.timerData),
+        });
+        const responseSuccess = await response.ok;
+        setResponseOk(responseSuccess);
+    };
+
+    return (
+        <>
+            <p>
+                Experimental! Posts the form data to a URL so you can
+                programmatically set timers
+            </p>
+            <p>
+                Not aware of any endpoints that accept this data at the minute
+            </p>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+                {() => (
+                    <Form className="grid">
+                        <div className="field food-item">
+                            <Field
+                                className="item border"
+                                name="url"
+                                type="url"
+                                placeholder="https://example.com/set-timers"
+                                pattern="https?://.*"
+                                size="30"
+                                required
+                            />
+                            <label htmlFor="url">
+                                URL you'd like to POST the data to
+                            </label>
+                        </div>
+                        <button type="submit" className="item">
+                            Post!
+                        </button>
+                        {responseOk && <p>Success!</p>}
+                    </Form>
+                )}
+            </Formik>
+        </>
+    );
 }
